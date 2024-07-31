c890256242e601ee8edd7810bef7cdd003a0081b
tahfeezmizan <mizanahmed5316@gmail.com>
2024-07-31 22:57:06 +0600

    create sing up page

---
+++ src/app/login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loginpage = () => {
+    return (
+        <div>
+        login Page
+        </div>
+    );
+};
+
+export default Loginpage;
---
+++ src/app/singup/page.jsx
@@ -0,0 +1,143 @@
+"use client";
+
+import React, { useState } from 'react';
+import singUpImg from '../../../public/login-bg.jpg';
+import Link from 'next/link';
+
+const SingUppage = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (formData.password !== formData.confirmPassword) {
+            alert("Passwords do not match!");
+            return;
+        }
+        console.log(formData);
+        // Reset form
+        setFormData({
+            name: '',
+            email: '',
+            password: '',
+            confirmPassword: ''
+        });
+    };
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    return (
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8  '>
+            <div className="grid grid-cols-1 lg:grid-cols-2 border rounded-2xl overflow-hidden shadow-xl">
+                <div className="col-span-1" style={{
+                    backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url(${singUpImg.src})`,
+                    backgroundPosition: 'center',
+                    backgroundSize: 'cover',
+                    backgroundRepeat: 'no-repeat'
+                }}>
+                    {/* <img src={singUpImg.src} className='bg-cover' alt="" /> */}
+                </div>
+                <div className="col-span-1 ">
+                    <div className="py-16 px-10">
+                        <h2 className="text-4xl text-center font-Roboto font-bold mb-8">Sign Up</h2>
+                        <form onSubmit={handleSubmit} className="space-y-4">
+                            <div>
+                                <label htmlFor="name" className="block text-sm font-medium text-gray-700">Name</label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    value={formData.name}
+                                    onChange={handleChange}
+                                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="email" className="block text-sm font-medium text-gray-700">Email</label>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    value={formData.email}
+                                    onChange={handleChange}
+                                    className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    required
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block text-sm font-medium text-gray-700">Password</label>
+                                <div className="relative">
+                                    <input
+                                        type={showPassword ? "text" : "password"}
+                                        name="password"
+                                        id="password"
+                                        value={formData.password}
+                                        onChange={handleChange}
+                                        className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                        required
+                                    />
+                                    <button
+                                        type="button"
+                                        onClick={togglePasswordVisibility}
+                                        className="absolute inset-y-0 right-0 px-3 flex items-center text-sm leading-5"
+                                    >
+                                        {showPassword ? "Hide" : "Show"}
+                                    </button>
+                                </div>
+                            </div>
+                            <div>
+                                <label htmlFor="confirmPassword" className="block text-sm font-medium text-gray-700">Confirm Password</label>
+                                <div className="relative">
+                                    <input
+                                        type={showPassword ? "text" : "password"}
+                                        name="confirmPassword"
+                                        id="confirmPassword"
+                                        value={formData.confirmPassword}
+                                        onChange={handleChange}
+                                        className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                        required
+                                    />
+                                    <button
+                                        type="button"
+                                        onClick={togglePasswordVisibility}
+                                        className="absolute inset-y-0 right-0 px-3 flex items-center text-sm leading-5"
+                                    >
+                                        {showPassword ? "Hide" : "Show"}
+                                    </button>
+                                </div>
+                            </div>
+                            <div>
+                                <button
+                                    type="submit"
+                                    className="w-full py-3 text-center bg-gradient-to-r from-[#ffbe30] to-[#ff5007] text-white font-Roboto text-base uppercase rounded-full font-bold hover:bg-yellow-500 duration-300"
+                                >
+                                    Sign Up
+                                </button>
+                            </div>
+                        </form>
+
+                        <p className="text-lg font-OpenSans font-semibold text-center pt-6">Have an Account? <Link className='' href={`/login`}>Login</Link></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SingUppage;
