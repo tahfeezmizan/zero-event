aad66d7ce19abea43f4a92b86a40935017b6bcfb
tahfeezmizan <mizanahmed5316@gmail.com>
2024-07-31 12:39:58 +0600

    complete navbar with search bar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,11 +1,35 @@
 "use client"
 import Link from 'next/link';
+import { RxCross2 } from "react-icons/rx";
+import { RiMenu3Fill, RiSearchLine } from "react-icons/ri";
 import { usePathname, useRouter } from 'next/navigation';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Navbar = () => {
+    const [isClick, setIsclick] = useState(false);
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
+    const [searchText, setSearchText] = useState('');
     const pathName = usePathname();
-    const route = useRouter()
+    const route = useRouter();
+
+    const toggleNavbar = () => {
+        setIsclick(!isClick)
+    }
+
+    const toggleSearch = () => {
+        setIsSearchOpen(!isSearchOpen);
+    }
+
+    const handleSearchChange = (e) => {
+        setSearchText(e.target.value);
+    }
+
+    const handleSearchSubmit = (e) => {
+        e.preventDefault();
+        console.log(searchText);
+        setSearchText('');
+        setIsSearchOpen(false);
+    }
 
     const links = [
         {
@@ -30,7 +54,7 @@ const Navbar = () => {
         },
     ]
 
-    const HandleLogin = () => {
+    const handleLogin = () => {
         route.push('/login')
     }
 
@@ -50,18 +74,86 @@ const Navbar = () => {
         )
 
     return (
-        <div className="w-4/5 mx-auto py-7 text-lg">
-            <nav className="flex justify-between">
-                <h3 className="text-3xl">NextApp</h3>
-                <ul className="flex gap-4 items-center ">
-                    {
-                        links.map((link) => <Link className={`${pathName === link.href && 'text-red-500 font-medium'}`} href={link.href}>{link.title}</Link>)
-                    }
-                    <button onClick={HandleLogin} className='bg-black text-white py-1 px-3'>Login</button>
-                </ul>
+        <nav className="bg-black">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="flex items-center justify-between h-16">
+                    <div className="flex items-center ">
+                        <div className="flex-shrink-0">
+                            <a href="/" className="text-white text-2xl">
+                                Logo
+                            </a>
+                        </div>
+                    </div>
+                    <div className="hidden lg:block">
+                        <div className="ml-4 flex justify-center items-center space-x-6">
+                            {
+                                links.map((link, index) => (
+                                    <Link key={index} className={` ${pathName === link.href ? 'text-red-500 font-medium' : "text-white font-medium"} `} href={link.href}>{link.title}</Link>
+                                ))
+                            }
 
-            </nav>
-        </div>
+                            <div className="">
+                                <form onSubmit={handleSearchSubmit} className="relative">
+                                    {isSearchOpen && (
+                                        <input
+                                            type="text"
+                                            value={searchText}
+                                            onChange={handleSearchChange}
+                                            className="px-3 py-1 outline-none bg-white text-black rounded-sm"
+                                            placeholder="Search..."
+                                        />
+                                    )}
+                                    <button type="button" onClick={toggleSearch} className="absolute -top-2 bottom-0 -left-2">
+                                        <RiSearchLine className="text-white text-2xl" />
+                                    </button>
+                                </form>
+                            </div>
+
+                            <button onClick={handleLogin} className='text-white py-1 px-3'>Login</button>
+                        </div>
+                    </div>
+
+                    <div className="lg:hidden flex items-center">
+                        <button
+                            className="inline-flex items-center justify-center p-2 text-3xl rounded-md text-white md:text-white hover:text-white focus:outline-none transition ease-in-out focus:ring-inset focus:ring-white"
+                            onClick={toggleNavbar}
+                        >
+                            {
+                                isClick ? (
+                                    <RxCross2 />
+                                ) : (
+                                    <RiMenu3Fill />
+                                )
+                            }
+                        </button>
+                    </div>
+                </div>
+            </div>
+            {isClick && (
+                <div className="lg:hidden flex flex-col items-center space-y-3 py-4">
+                    {
+                        links.map((link, index) => (
+                            <Link key={index} className={` ${pathName === link.href ? 'text-red-500 font-medium' : "text-white font-medium"} `} href={link.href}>{link.title}</Link>
+                        ))
+                    }
+                    <button onClick={handleLogin} className='text-white py-1 px-3'>Login</button>
+                    <form onSubmit={handleSearchSubmit} className="relative w-full flex justify-center">
+                        {isSearchOpen && (
+                            <input
+                                type="text"
+                                value={searchText}
+                                onChange={handleSearchChange}
+                                className="pl-2 pr-8 py-1 bg-white text-black rounded-md w-3/4"
+                                placeholder="Search..."
+                            />
+                        )}
+                        <button type="button" onClick={toggleSearch} className="absolute pb-5">
+                            <RiSearchLine className="text-white text-2xl" />
+                        </button>
+                    </form>
+                </div>
+            )}
+        </nav>
     );
 };
 
